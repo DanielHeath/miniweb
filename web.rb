4e9df968585e680164c5ad4e258d3313e2b0e22c
Daniel Heath <daniel.r.heath@gmail.com>
2012-12-19 16:35:17 +1100

    Add sample links and redirect

--- web.rb
+++ web.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  redirect '/hello'
+end
+
+get '/hello' do
+  "Hello.<br/> <a href='/goodbye'>Say goodbye</a>"
+end
+
+get '/goodbye' do
+  "Goodbye.<br/> <a href='/hello'>Say hello</a>"
 end
 
