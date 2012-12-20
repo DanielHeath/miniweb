469639cc4bcf5263d9084ef9d892ca161480e7ed
Daniel Heath <daniel.r.heath@gmail.com>
2012-12-20 11:45:32 +1100

    Add view example

--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require './heroku_config'
 
 get '/' do
-  redirect '/hello'
+  redirect '/erb_views'
 end
 
 get '/hello' do
@@ -13,13 +13,17 @@ get '/goodbye' do
   "Goodbye.<br/> <a href='/hello'>Say hello</a>"
 end
 
+get '/erb_views' do
+  erb :sample_view
+end
+
 class StoredString
   include Mongoid::Document # Lets it be stored
   field :content, type: String
 end
 
-get '/mongo' do
-  form = "<form action='/mongo' method='post'><input name='content'></input><input type='submit' value='submit'></input></form>"
+get '/chat' do
+  form = "<a href='/'>Home</a><br/><form action='/new_chat_message' method='post'><input name='content'></input><input type='submit' value='submit'></input></form>"
   list = []
   StoredString.each do |s|
     list << "<p>#{s.content}</p>"
@@ -28,7 +32,7 @@ get '/mongo' do
   form + list.reverse.join
 end
 
-post '/mongo' do
+post '/new_chat_message' do
   StoredString.create! :content => params['content']
-  redirect '/mongo'
+  redirect '/chat'
 end
